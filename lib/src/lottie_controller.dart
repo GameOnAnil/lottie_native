05ae191a7ec511e891e58514b3f58bfd5dd31424
Anil Thapa <anil.thapa@8squarei.com>
2025-04-22 16:45:59 +0545

    refactor: remove stop logic

--- lib/src/lottie_controller.dart
+++ lib/src/lottie_controller.dart
@@ -27,7 +27,7 @@ class LottieController {
   // Main cleanup method
   Future<void> dispose() async {
     try {
-      await stop(); // Optional: stop animation
+      // await stop(); // Optional: stop animation
       await _channel.invokeMethod('clearCache');
     } catch (e) {
       // Log error or ignore if already disposed
